67bf0421eba91d35ae79b5701d20b8e276849b35
slushy <107578323+blue-slushy9@users.noreply.github.com>
2024-11-04 15:04:27 -0800

    Create coffees.cpp

---
+++ coffees.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+#include <map>
+
+std::map<int, std::string> create_map() {
+	std::map<int, std::string> coffees;
+
+    coffees[1] = "Espresso";
+    coffees[2] = "Americano";
+    coffees[3] = "Cappuccino";
+    coffees[4] = "Latte";
+    return coffees;
+}
+
+/*
+print_selection(coffees) {
+	cout 
+}
+*/
+
+void switch_coffee(int num, std::map<int, std::string> coffees) {
+	switch(num) {
+		case 1:
+		    cout << coffees[1];
+		    break;
+		case 2: 
+		    cout << coffees[2];
+		    break;
+		case 3:
+		    cout << coffees[3];
+		    break;
+		case 4: 
+		    cout << coffees[4];
+		    break;
+		default:
+		    cout << "Invalid entry, please enter an integer between 1 - 4.";
+	}
+}
+
+int main() {
+    int choice;
+    cin >> choice;
+    std::map<int, std::string> coffees = create_map();
+    switch_coffee(choice, coffees);
+
+}
