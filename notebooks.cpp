780088898feeb1e83a962f9c6432c5f4f864b774
blue-slushy9 <ra.cruz971@gmail.com>
2024-11-07 14:34:42 -0800

    Finished adding features to notebooks.cpp, it looks okay but the compiler and linter might return some errors.

--- notebooks.cpp
+++ notebooks.cpp
@@ -1,9 +1,19 @@
-// One of the small SoloLearn exercises
+/* 
+One of the small SoloLearn exercises, it's a simple program that calculates
+how many notebooks the user can afford to buy, as well as their remaining
+bank balance after doing so 
+*/
 
 #include <iostream>
 using namespace std;
 
 // FUNCTION DECLARATIONS
+
+// Declare function that prompts user for their balance
+float get_balance();
+
+// Declare function that prompts user for how much they want to pay
+float get_price();
 
 // Calculate number of notebooks you can afford to buy
 // b = balance, p = price
@@ -14,10 +24,10 @@ int calc_notebooks(float b, float p);
 void calc_balance(float b, float p, int n);
 
 int main() {
-    // Starting account balance
-    float balance = 2452.4f;
+    // Call function that prompts user for current balance
+    float balance = get_balance();
     // Price for each notebook
-    float price = 259.99f;
+    float price = get_price();
 
     // Calculate the number of notebooks you can afford and output result
     int notebooks = calc_notebooks(balance, price);
@@ -28,6 +38,22 @@ int main() {
 }
 
 // FUNCTION DEFINITIONS
+
+// Define function that prompts user for their balance
+float get_balance() {
+    float num;
+    cout << ("Please enter your current balance: ");
+    cin >> num;
+    return num;
+}
+
+// Declare function that prompts user for how much they want to pay
+float get_price() {
+    float num;
+    cout << ("Please enter the price you are looking to pay: ");
+    cin >> num;
+    return num;
+}
 
 // Calculate number of notebooks you can afford to buy
 int calc_notebooks(float b, float p) {
