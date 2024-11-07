7a35f583049dea0afc57bc8ae89016d12b1a23ba
blue-slushy9 <ra.cruz971@gmail.com>
2024-11-06 16:03:29 -0800

    Almost finished the coffees.cpp program, might just need to debug and refactor a little more.

--- coffees.cpp
+++ coffees.cpp
@@ -1,43 +1,68 @@
 // One of the small SoloLearn exercises
 
-#include <iostream> 
-using namespace std;
+#include <iostream>
 #include <map>
+using namespace std;
 
-std::map<int, std::string> create_map() {
-	std::map<int, std::string> coffees;
+// Declare function that creates map of the different coffees
+map<int, string> create_map();
 
+// Declare switch function for selecting a coffee
+void switch_coffee(int num, map<int, string> coffees);
+
+// Declare function that prompts user for their choice in coffee
+void choose_coffee();
+
+int main() {
+    map<int, string> coffees = create_map();
+	int choice = choose_coffee();
+}
+
+// Define function that creates map of the different coffees
+map<int, string> create_map() {
+	// Key is int, Value is string
+	map<int, string> coffees;
+    (
     coffees[1] = "Espresso";
     coffees[2] = "Americano";
     coffees[3] = "Cappuccino";
     coffees[4] = "Latte";
-    // Curious whether the below would work...
-	//string coffees_prefix = "std::map<int, std::string> ";
-    return coffees;//, coffees_prefix;
+	)
+    return coffees;
 }
 
-void switch_coffee(int num, std::map<int, std::string> coffees) {
+// Define switch function for selecting a coffee
+string switch_coffee(int num, map<int, string> coffees) {
 	switch(num) {
 		case 1:
-		    cout << coffees[1];
-		    break;
+		    return coffees[1];
 		case 2: 
-		    cout << coffees[2];
-		    break;
+		    return coffees[2];
 		case 3:
-		    cout << coffees[3];
-		    break;
+		    return coffees[3];
 		case 4: 
-		    cout << coffees[4];
-		    break;
+		    return coffees[4];
 		default:
-		    cout << "Invalid entry, please enter an integer between 1 - 4.";
+		    cout << ("Invalid entry, please enter an integer between 1 - 4.");
+			// Call function recursively until valid entry is submitted
+			switch_coffee(num, coffees);
 	}
 }
 
-int main() {
-    int choice;
-    cin >> choice;
-    std::map<int, std::string> coffees = create_map();
-    switch_coffee(choice, coffees);
+// Define function that prompts user for their choice in coffee
+void choose_coffee() {
+	cout << ("Here are the menu items: ") << endl;
+	// Iterate over contents of coffees map and print menu,
+	// 'const auto& pair' automatically detects the key-value types
+	for (const auto& pair : coffees) {
+		// 'pair.first' prints key, 'pair.second' prints value
+		cout << pair.first << ". " << pair.second << endl;
+	}
+	cout << ("Please enter the number of the coffee you want now: ");
+	int order;
+	cin >> order;
+	// Call switch_coffees function
+	string coffee = switch_coffee(order, map<int, string> coffees);
+	// Confirm the order
+	cout << "You chose a " + coffee + ", sounds delicious! Enjoy." << endl;
 }
