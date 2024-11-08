3efca128b47c09201c33e1c7a783216020ce4858
blue-slushy9 <ra.cruz971@gmail.com>
2024-11-07 16:42:46 -0800

    Started working on refactoring tickets.cpp, still have a lot left to do with it.

--- tickets.cpp
+++ tickets.cpp
@@ -1,30 +1,42 @@
-// One of the small SoloLearn projects
+// One of the small SoloLearn projects, this is a simple program that issues
+// a different type of ticket based on the user's age
 
 #include <iostream>
 using namespace std;
 
-int take_input() {
+// Declare function that takes age as input and capture it in a variable
+int enter_age();
+
+// Declare function that prints ticket type based on the age entered
+void check_age(int num);
+
+int main() {
+    int age = enter_age();
+    check_age(age);
+}
+
+// Define function that takes age as input and capture it in a variable
+int enter_age() {
 	int num;
+	cout << ("Please enter your age now: ");
 	cin >> num;
 	return num;
 }
 
+// Define function that prints ticket type based on the age entered
 void check_age(int num) {
 	if (num >= 0 && num <= 3) {
-		cout << "Free";
+		cout << ("Free");
 	}
 	else if (num >= 4 && num <= 6) {
-		cout << "Discounted";
+		cout << ("Discounted");
 	}
 	else if (num >= 7) {
-		cout << "Normal";
+		cout << ("Normal");
 	}
 	else {
-		cout << "Invalid number, please enter a real age";
+		cout << ("Invalid number, please enter a real age");
+		// Call function recursively until valid age is entered
+		check_age(num);
 	}
-}
-
-int main() {
-    int age = take_input();
-    check_age(age);
 }
