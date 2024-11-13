0149af76bdeade28c086d1e9671d8f254afbf120
blue-slushy9 <ra.cruz971@gmail.com>
2024-11-12 16:37:55 -0800

    Added the '#include <cctype>' state for the tolower() function, etc.

--- tickets.cpp
+++ tickets.cpp
@@ -2,6 +2,8 @@
 // a different type of ticket based on the user's age
 
 #include <iostream>
+// cctype includes the tolower() method
+#include <cctype>
 using namespace std;
 
 // Declare function that takes age as input and capture it in a variable
@@ -17,7 +19,7 @@ int main() {
     int age = enter_age();
     // check_age() returns the price tier based on the age that is entered
     string price = check_age(age);
-    confirm_ticket();
+    confirm_ticket(age, price);
 }
 
 // Define function that takes age as input and capture it in a variable
@@ -51,15 +53,16 @@ string check_age(int num) {
 }
 
 // Define function that prints ticket type to terminal and prompts user for confirmation
-void confirm_ticket(int num, string ) {
+void confirm_ticket(int num, string type) {
 	string message = "Okay, since the age of the person is " + age + ", your\n"
-	                 "ticket will be " + num ". Is this correct? [Y/n] ";
+	                 "ticket will be " + type + ". Is this correct? [Y/n] ";
 	// Print above message to terminal
 	cout << message;
 	// Declare variable that will store user's Yes or No response
 	char y_n;
 	cin >> y_n;
-
+	// Convert the input to lower-case for easier processing
+    y_n = tolower(y_n);
 	if (y_n == "y") {
 
 	}
@@ -68,6 +71,7 @@ void confirm_ticket(int num, string ) {
 	}
     else {
 		cout << "Please enter a valid response." << endl;
-		confirm_ticket()
+		// Call function recursively until a valid response is entered
+		confirm_ticket(age, price);
 	}
 }
