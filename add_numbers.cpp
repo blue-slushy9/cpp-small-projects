37a6d140056bf868d43b35668c36ca2b603a48a4
blue-slushy9 <ra.cruz971@gmail.com>
2024-11-15 19:17:40 -0800

    Almost finished with add_numbers.cpp, there is just an issue with the format() method.

--- add_numbers.cpp
+++ add_numbers.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <format>
 using namespace std;
 
 // Declare the function that takes the base number as input, then returns it
@@ -7,10 +9,14 @@ int enter_number();
 // Declare the function that performs the addition, then returns the sum
 int add_numbers(int n);
 
+// Declare the function that will print the final value to the terminal
+string final_value(int num);
+
 int main() {
     int number = enter_number();
     int sum = add_numbers(number);
-    cout << "Final value: " + sum; << endl;
+    string final = final_value(sum);
+    cout << final << endl;
 }
 
 // Define the function that takes the base number as input, then returns it
@@ -26,10 +32,15 @@ int enter_number() {
 int add_numbers(int n) {
     // Initialize sum at 0
     int sum = 0;
-    // We start by adding 1 to the sum, and end by adding the number that was
-    // entered to the sum
+    // We start by adding 1 to the sum, and end by adding n to the sum
     for (int i=1; i<=n; i++) {
         sum+=i;
     }
     return sum;
+}
+
+// Define the function that will print the final value to the terminal
+string final_value(int num) {
+    string message = format("Final value: {}", num);
+    return message;
 }
