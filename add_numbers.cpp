d1a6a4a78f9c24426c1754ae7da196f5cdc917a4
blue-slushy9 <ra.cruz971@gmail.com>
2024-11-15 16:33:14 -0800

    May have finished add_numbers.cpp, still have to check in the linter, compiler, etc.

--- add_numbers.cpp
+++ add_numbers.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int enter_number() {
-	int num;
-	cin >> num;
-	return num;
-}
+// Declare the function that takes the base number as input, then returns it
+int enter_number();
 
-int add_numbers(int n) {
-    int sum = 0;
-    for (int i=1; i<=n; i++) {
-        sum+=i;
-    }
-    return sum;
-}
+// Declare the function that performs the addition, then returns the sum
+int add_numbers(int n);
 
 int main() {
     int number = enter_number();
     int sum = add_numbers(number);
-    cout << sum;
+    cout << "Final value: " + sum; << endl;
+}
+
+// Define the function that takes the base number as input, then returns it
+int enter_number() {
+	int num;
+    cout << "Please enter your base integer on which the addition calculations "
+         << "will be performed: ";
+	cin >> num;
+	return num;
+}
+
+// Define the function that performs the addition, then returns the sum
+int add_numbers(int n) {
+    // Initialize sum at 0
+    int sum = 0;
+    // We start by adding 1 to the sum, and end by adding the number that was
+    // entered to the sum
+    for (int i=1; i<=n; i++) {
+        sum+=i;
+    }
+    return sum;
 }
